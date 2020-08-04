f75bc05a016dd0ea70c191e44beb1c43ea5b8588
krawieck <fil.krawczyk@gmail.com>
2020-08-04 17:27:44 +0200

    Add `search` and `listCategories` #6
    Add endpoints for:
    * `GET /categories`
    * `POST /search`

--- lib/client/client.dart
+++ lib/client/client.dart
@@ -18,6 +18,26 @@ class V1 {
   String instanceUrl;
   UserEndpoint user;
   V1(this.instanceUrl) : user = UserEndpoint(instanceUrl);
+
+  /// GET /categories
+  /// https://dev.lemmy.ml/docs/contributing_websocket_http_api.html#list-categories
+  String listCategories() {
+    throw UnimplementedError();
+  }
+
+  /// POST /search
+  /// https://dev.lemmy.ml/docs/contributing_websocket_http_api.html#search
+  String search({
+    @required String q,
+    @required String type,
+    String communityId,
+    @required String sort,
+    int page,
+    int limit,
+    String auth,
+  }) {
+    throw UnimplementedError();
+  }
 }
 
 class UserEndpoint {
