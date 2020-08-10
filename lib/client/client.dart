13522248d4cd662a4a2f6fc75d51400810b028de
krawieck <fil.krawczyk@gmail.com>
2020-08-11 00:10:17 +0200

    Add exports

--- lib/client/client.dart
+++ lib/client/client.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/foundation.dart' show required;
 import 'v1/main.dart';
 
+export 'v1/comment_endpoint.dart';
+export 'v1/post_endpoint.dart';
+export 'v1/user_endpoint.dart';
+
 class LemmyAPI {
   /// url of this lemmy instance
   String instanceUrl;
@@ -8,7 +12,7 @@ class LemmyAPI {
   V1 v1;
 
   /// initialize lemmy api instance
-  LemmyAPI({@required this.instanceUrl})
+  LemmyAPI(this.instanceUrl)
       : assert(instanceUrl != null),
         v1 = V1(instanceUrl);
 }
