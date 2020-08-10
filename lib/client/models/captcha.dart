101c7c4afc09d360e4cc0f4d8df68b7bd1860a97
shilangyu <xmarcinmarcin@gmail.com>
2020-08-10 12:22:28 +0000

    made captcha json-serializable

--- lib/client/models/captcha.dart
+++ lib/client/models/captcha.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+part 'captcha.g.dart';
+
+/// based on https://dev.lemmy.ml/docs/contributing_websocket_http_api.html#get-captcha
+@JsonSerializable(fieldRename: FieldRename.snake, createToJson: false)
 class Captcha {
   final String png;
 
   /// can be null
   final String wav;
   final String uuid;
-  Captcha({@required this.png, this.wav, @required this.uuid});
+  
+  const Captcha({
+    this.png,
+    this.wav,
+    this.uuid
+  });
+
+  factory Captcha.fromJson(Map<String, dynamic> json) =>
+      _$CaptchaFromJson(json);
 }
