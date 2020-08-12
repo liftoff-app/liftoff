141b9296614e37ce84b29bd3df5663833637f533
shilangyu <xmarcinmarcin@gmail.com>
2020-08-12 21:00:31 +0200

    export enums as well

--- lib/client/client.dart
+++ lib/client/client.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/foundation.dart' show required;
 import 'v1/main.dart';
 
-export 'v1/comment_endpoint.dart';
-export 'v1/post_endpoint.dart';
-export 'v1/user_endpoint.dart';
+export 'v1/main.dart';
 
 class LemmyAPI {
   /// url of this lemmy instance
