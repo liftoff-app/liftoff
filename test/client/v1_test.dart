9a9a66cd17097c65b7cd6f2700dc63e73cd190d4
shilangyu <xmarcinmarcin@gmail.com>
2020-08-13 16:32:02 +0200

    tests for main.dart

--- test/client/v1_test.dart
+++ test/client/v1_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lemmur/client/client.dart';
 
-// this is a mock exception
+// these are mock exceptions
 // should be removed as soon as the real one is implemented
-class LemmyAPIException implements Exception {
+class NotLoggedInException implements Exception {
   final String _message;
 
-  LemmyAPIException(this._message);
+  NotLoggedInException(this._message);
 
   @override
   String toString() => _message;
@@ -20,19 +20,57 @@ void main() {
       test("correctly fetches", () async {
         final res = await lemmy.getPosts(
             type: PostListingType.all, sort: SortType.active);
-
         expect(res.length, 10);
       });
 
-      test("gracefully fails", () async {
-        try {
+      test("forbids illegal numbers", () async {
+        expect(() async {
           await lemmy.getPosts(
-              type: PostListingType.all, sort: SortType.active, page: -1);
-        } on LemmyAPIException catch (_) {
-          return;
-        }
+              type: PostListingType.all, sort: SortType.active, page: 0);
+        }, throwsA(isInstanceOf<AssertionError>()));
 
-        throw TestFailure("Did not catch the error");
+        expect(() async {
+          await lemmy.getPosts(
+              type: PostListingType.all, sort: SortType.active, limit: -1);
+        }, throwsA(isInstanceOf<AssertionError>()));
+      });
+    });
+
+    group("listCategories", () {
+      test("correctly fetches", () async {
+        await lemmy.listCategories();
+      });
+    });
+
+    group("search", () {
+      test("correctly fetches", () async {
+        final res = await lemmy.search(
+            type: SearchType.all, q: "asd", sort: SortType.active);
+
+        expect(res.type, SortType.active.value);
+      });
+
+      test("forbids illegal numbers", () async {
+        expect(() async {
+          await lemmy.search(
+              type: SearchType.all, q: "asd", sort: SortType.active, page: 0);
+        }, throwsA(isInstanceOf<AssertionError>()));
+
+        expect(() async {
+          await lemmy.search(
+              type: SearchType.all, q: "asd", sort: SortType.active, limit: -1);
+        }, throwsA(isInstanceOf<AssertionError>()));
+      });
+
+      test("handles invalid tokens", () async {
+        expect(() async {
+          await lemmy.search(
+            type: SearchType.all,
+            q: "asd",
+            sort: SortType.active,
+            auth: "asd",
+          );
+        }, throwsA(isInstanceOf<NotLoggedInException>()));
       });
     });
   });
