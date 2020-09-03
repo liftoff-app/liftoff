11a2e40dfd65f8224d260fec9c4ec20cc25dff12
krawieck <fil.krawczyk@gmail.com>
2020-09-03 12:23:46 +0200

    Change `log` to `log10` and remove useless code

--- lib/util/hot_rank.dart
+++ lib/util/hot_rank.dart
@@ -1,13 +1,15 @@
-// import 'dart:core' show ab;
-import 'dart:math' show log, max, pow;
+import 'dart:math' show log, max, pow, ln10;
 
 import 'package:lemmy_api_client/lemmy_api_client.dart';
 
 double _calculateHotRank(int score, DateTime time) {
+  log10(num x) => log(x) / ln10;
+
   final elapsed = (time.difference(DateTime.now()).inMilliseconds).abs() / 36e5;
 
   // log instead of log10 in web version. does log == log10?
-  return (10000 * log(max(1, 3 + score))) / pow(elapsed + 2, 1.8);
+
+  return (10000 * log10(max(1, 3 + score))) / pow(elapsed + 2, 1.8);
 }
 
 extension CommentHotRank on CommentView {
