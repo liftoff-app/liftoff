869bb0eccc3d3a88143019725b6d9304e89cdb4d
krawieck <fil.krawczyk@gmail.com>
2020-09-03 14:00:06 +0200

    remove useless import

--- lib/pages/full_post.dart
+++ lib/pages/full_post.dart
@@ -2,7 +2,6 @@ import 'package:esys_flutter_share/esys_flutter_share.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:lemmy_api_client/lemmy_api_client.dart';
-import 'package:lemmy_api_client/src/models/post.dart';
 
 import '../widgets/comment_section.dart';
 import '../widgets/post.dart';
