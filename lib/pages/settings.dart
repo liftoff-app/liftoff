b704502c7d97bb6507e03b20021e8b4417849292
krawieck <fil.krawczyk@gmail.com>
2020-09-03 22:20:32 +0200

    Convert `Columns` to `ListView`s where appropriate

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -58,11 +58,13 @@ class _AppearanceConfig extends StatelessWidget {
         centerTitle: true,
       ),
       body: Observer(
-        builder: (ctx) => Column(
+        builder: (ctx) => ListView(
           children: [
-            Text(
-              'Theme',
-              style: theme.textTheme.headline6,
+            Center(
+              child: Text(
+                'Theme',
+                style: theme.textTheme.headline6,
+              ),
             ),
             for (final theme in ThemeMode.values)
               RadioListTile<ThemeMode>(
@@ -98,13 +100,14 @@ class _AccountsConfig extends StatelessWidget {
           var accountsStore = ctx.watch<AccountsStore>();
           var theme = Theme.of(context);
 
-          return Column(
-            mainAxisSize: MainAxisSize.min,
+          return ListView(
             children: [
               for (var entry in accountsStore.users.entries) ...[
-                Text(
-                  entry.key,
-                  style: theme.textTheme.subtitle2,
+                Center(
+                  child: Text(
+                    entry.key,
+                    style: theme.textTheme.subtitle2,
+                  ),
                 ),
                 for (var username in entry.value.keys) ...[
                   ListTile(
