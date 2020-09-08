81b10cf3e3b60bedfe6f2e136fda28179ed8b885
shilangyu <xmarcinmarcin@gmail.com>
2020-09-08 16:37:58 +0200

    removed unnecessary dividers

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -143,9 +143,8 @@ class _AccountsConfig extends HookWidget {
                     );
                   },
                 ),
-                Divider(),
               ]
-            ]..removeLast(), // removes trailing Divider
+            ],
           );
         },
       ),
