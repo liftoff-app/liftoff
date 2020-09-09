bb789b3ac9ea1c0d9daef8360b72a211d7bf1dc0
krawieck <fil.krawczyk@gmail.com>
2020-09-09 19:23:41 +0200

    Cleanup and a addition of couple comments

--- lib/pages/full_post.dart
+++ lib/pages/full_post.dart
@@ -19,84 +19,71 @@ class FullPostPage extends HookWidget {
   FullPostPage.fromPostView(this.post)
       : fullPost = LemmyApi(post.instanceUrl).v1.getPost(id: post.id);
 
-  void sharePost() => Share.text('Share post', post.apId, 'text/plain');
-
-  void savePost() {
-    //
-  }
-
   @override
   Widget build(BuildContext context) {
     final fullPostSnap = useFuture(this.fullPost);
+
+    // FALLBACK VIEW
+
+    if (!fullPostSnap.hasData && this.post == null) {
+      return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (fullPostSnap.hasError)
+                Text(fullPostSnap.error.toString())
+              else
+                const CircularProgressIndicator(),
+            ],
+          ),
+        ),
+      );
+    }
+
+    // VARIABLES
+
+    final post = fullPostSnap.hasData ? fullPostSnap.data.post : this.post;
+
     final fullPost = fullPostSnap.data;
 
-    final savedIcon = () {
-      if (fullPostSnap.hasData) {
-        if (fullPost.post.saved == null || !fullPost.post.saved) {
-          return Icons.bookmark_border;
-        } else {
-          return Icons.bookmark;
-        }
-      }
+    final savedIcon = (post.saved == null || !post.saved)
+        ? Icons.bookmark_border
+        : Icons.bookmark;
 
-      if (post != null) {
-        if (post.saved == null || !post.saved) {
-          return Icons.bookmark_border;
-        } else {
-          return Icons.bookmark;
-        }
-      }
+    // FUNCTIONS
 
-      return Icons.bookmark_border;
-    }();
+    sharePost() => Share.text('Share post', post.apId, 'text/plain');
+
+    savePost() {
+      print('SAVE POST');
+    }
 
     return Scaffold(
-      appBar: AppBar(
-        leading: BackButton(),
-        actions: [
-          IconButton(icon: Icon(Icons.share), onPressed: sharePost),
-          IconButton(icon: Icon(savedIcon), onPressed: savePost),
-          IconButton(
-              icon: Icon(Icons.more_vert), onPressed: () {}), // TODO: more menu
-        ],
-      ),
-      body: fullPostSnap.hasData || post != null
-          // FUTURE SUCCESS
-          ? ListView(
-              physics: const AlwaysScrollableScrollPhysics(),
-              children: [
-                if (fullPostSnap.hasData)
-                  Post(fullPost.post, fullPost: true)
-                else if (post != null)
-                  Post(post, fullPost: true)
-                else
-                  CircularProgressIndicator(),
-                if (fullPostSnap.hasData)
-                  CommentSection(fullPost.comments,
-                      postCreatorId: fullPost.post.creatorId)
-                else
-                  Container(
-                    child: Center(child: CircularProgressIndicator()),
-                    padding: EdgeInsets.only(top: 40),
-                  ),
-              ],
-            )
-          : fullPostSnap.hasError
-              // FUTURE FAILURE
-              ? Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.error, size: 30),
-                      Padding(padding: EdgeInsets.all(5)),
-                      Text('ERROR: ${fullPostSnap.error.toString()}'),
-                    ],
-                  ),
-                )
-              // FUTURE IN PROGRESS
-              : Container(
-                  child: Center(child: CircularProgressIndicator()),
-                  color: Theme.of(context).canvasColor),
-    );
+        appBar: AppBar(
+          leading: BackButton(),
+          actions: [
+            IconButton(icon: Icon(Icons.share), onPressed: sharePost),
+            IconButton(icon: Icon(savedIcon), onPressed: savePost),
+            IconButton(
+                icon: Icon(Icons.more_vert),
+                onPressed: () {}), // TODO: more menu
+          ],
+        ),
+        body: ListView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          children: [
+            Post(post, fullPost: true),
+            if (fullPostSnap.hasData)
+              CommentSection(fullPost.comments,
+                  postCreatorId: fullPost.post.creatorId)
+            else
+              Container(
+                child: Center(child: CircularProgressIndicator()),
+                padding: EdgeInsets.only(top: 40),
+              ),
+          ],
+        ));
   }
 }
