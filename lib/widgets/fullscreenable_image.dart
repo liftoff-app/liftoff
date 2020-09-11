1746cc0db5c8b8cc2ea61ff7871b422402a448ab
krawieck <fil.krawczyk@gmail.com>
2020-09-11 20:36:19 +0200

    mark props as required and move onTap to a method

--- lib/widgets/fullscreenable_image.dart
+++ lib/widgets/fullscreenable_image.dart
@@ -6,16 +6,22 @@ class FullscreenableImage extends StatelessWidget {
   final String url;
   final Widget child;
 
-  const FullscreenableImage({Key key, this.url, this.child}) : super(key: key);
+  const FullscreenableImage({
+    Key key,
+    @required this.url,
+    @required this.child,
+  }) : super(key: key);
+
+  _onTap(BuildContext c) {
+    Navigator.of(c).push(MaterialPageRoute(
+      builder: (context) => MediaViewPage(url),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => MediaViewPage(url),
-        ));
-      },
+      onTap: () => _onTap(context),
       child: Hero(
         tag: url,
         child: child,
