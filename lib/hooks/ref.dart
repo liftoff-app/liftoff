5c871da9ff4a389555f2b42a8df10f6db4936ce6
shilangyu <xmarcinmarcin@gmail.com>
2020-09-16 01:27:01 +0200

    remove use prefix from hook file

--- lib/hooks/ref.dart
+++ lib/hooks/ref.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class Ref<T> {
-  final T current;
-  const Ref(this.current);
+  T current;
+  Ref(this.current);
 }
 
+/// see React's useRef
 Ref<T> useRef<T>(T initialValue) => useMemoized(() => Ref(initialValue));
