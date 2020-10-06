fe494a3b0b4e4770f0878a93969ef20baa9a2a2f
krawieck <fil.krawczyk@gmail.com>
2020-10-06 15:58:58 +0200

    move clean up to lemmyApi extensions

--- lib/util/extensions/api.dart
+++ lib/util/extensions/api.dart
@@ -1,5 +1,7 @@
 import 'package:lemmy_api_client/lemmy_api_client.dart';
 
+import '../cleanup_url.dart';
+
 // Extensions to lemmy api objects which give a [.instanceUrl] getter
 // allowing for a convenient way of knowing from which instance did this
 // object come from
@@ -7,25 +9,27 @@ import 'package:lemmy_api_client/lemmy_api_client.dart';
 // TODO: change it to something more robust? regex?
 
 extension GetInstanceCommunityView on CommunityView {
-  String get instanceUrl => actorId.split('/')[2];
+  String get instanceUrl => extract(actorId);
 }
 
 extension GetInstanceUserView on UserView {
-  String get instanceUrl => actorId.split('/')[2];
+  String get instanceUrl => extract(actorId);
 }
 
 extension GetInstanceCommunityModeratorView on CommunityModeratorView {
-  String get instanceUrl => userActorId.split('/')[2];
+  String get instanceUrl => extract(userActorId);
 }
 
 extension GetInstancePostView on PostView {
-  String get instanceUrl => apId.split('/')[2];
+  String get instanceUrl => extract(apId);
 }
 
 extension GetInstanceUser on User {
-  String get instanceUrl => actorId.split('/')[2];
+  String get instanceUrl => extract(actorId);
 }
 
 extension GetInstanceCommentView on CommentView {
-  String get instanceUrl => apId.split('/')[2];
+  String get instanceUrl => extract(apId);
 }
+
+String extract(String s) => cleanUpUrl(s.split('/')[2]);
