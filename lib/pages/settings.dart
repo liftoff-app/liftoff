4917c84623a2102a298aa019bb66f0851310e1c4
shilangyu <xmarcinmarcin@gmail.com>
2020-10-25 21:33:44 +0000

    converted config store to change notifier

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
@@ -67,27 +66,25 @@ class AppearanceConfigPage extends HookWidget {
         title: Text('Appearance', style: theme.textTheme.headline6),
         centerTitle: true,
       ),
-      body: Observer(
-        builder: (ctx) => ListView(
-          children: [
-            _SectionHeading('Theme'),
-            for (final theme in ThemeMode.values)
-              RadioListTile<ThemeMode>(
-                value: theme,
-                title: Text(theme.toString().split('.')[1]),
-                groupValue: configStore.theme,
-                onChanged: (selected) {
-                  configStore.theme = selected;
-                },
-              ),
-            SwitchListTile(
-                title: Text('AMOLED dark mode'),
-                value: configStore.amoledDarkMode,
-                onChanged: (checked) {
-                  configStore.amoledDarkMode = checked;
-                })
-          ],
-        ),
+      body: ListView(
+        children: [
+          _SectionHeading('Theme'),
+          for (final theme in ThemeMode.values)
+            RadioListTile<ThemeMode>(
+              value: theme,
+              title: Text(theme.toString().split('.')[1]),
+              groupValue: configStore.theme,
+              onChanged: (selected) {
+                configStore.theme = selected;
+              },
+            ),
+          SwitchListTile(
+              title: Text('AMOLED dark mode'),
+              value: configStore.amoledDarkMode,
+              onChanged: (checked) {
+                configStore.amoledDarkMode = checked;
+              })
+        ],
       ),
     );
   }
@@ -185,100 +182,92 @@ class AccountsConfigPage extends HookWidget {
           ),
         ],
       ),
-      body: Observer(
-        builder: (ctx) {
-          final theme = Theme.of(context);
-
-          return ListView(
-            children: [
-              if (accountsStore.tokens.isEmpty)
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 100),
-                      child: FlatButton.icon(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          onPressed: () => showCupertinoModalPopup(
-                                context: context,
-                                builder: (_) => AddInstancePage(),
-                              ),
-                          icon: Icon(Icons.add),
-                          label: Text('Add instance')),
-                    ),
-                  ],
-                ),
-              for (final entry in accountsStore.tokens.entries) ...[
-                SizedBox(height: 40),
-                Slidable(
-                  actionPane: SlidableBehindActionPane(),
-                  secondaryActions: [
-                    IconSlideAction(
-                      closeOnTap: true,
-                      onTap: () => removeInstanceDialog(entry.key),
-                      icon: Icons.delete_sweep,
-                      color: Colors.red,
-                    ),
-                  ],
-                  key: Key(entry.key),
-                  child: Container(
-                    color: theme.canvasColor,
-                    child: ListTile(
-                      dense: true,
-                      contentPadding: EdgeInsets.only(left: 0, top: 0),
-                      title: _SectionHeading(entry.key),
-                    ),
-                  ),
-                ),
-                for (final username in entry.value.keys) ...[
-                  Slidable(
-                    actionPane: SlidableBehindActionPane(),
-                    key: Key('$username@${entry.key}'),
-                    secondaryActions: [
-                      IconSlideAction(
-                        closeOnTap: true,
-                        onTap: () => removeUserDialog(entry.key, username),
-                        icon: Icons.delete_sweep,
-                        color: Colors.red,
+      body: ListView(
+        children: [
+          if (accountsStore.tokens.isEmpty)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 100),
+                  child: FlatButton.icon(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                    ],
-                    child: Container(
-                      decoration: BoxDecoration(color: theme.canvasColor),
-                      child: ListTile(
-                        trailing: username ==
-                                accountsStore.defaultUsernameFor(entry.key)
+                      onPressed: () => showCupertinoModalPopup(
+                            context: context,
+                            builder: (_) => AddInstancePage(),
+                          ),
+                      icon: Icon(Icons.add),
+                      label: Text('Add instance')),
+                ),
+              ],
+            ),
+          for (final entry in accountsStore.tokens.entries) ...[
+            SizedBox(height: 40),
+            Slidable(
+              actionPane: SlidableBehindActionPane(),
+              secondaryActions: [
+                IconSlideAction(
+                  closeOnTap: true,
+                  onTap: () => removeInstanceDialog(entry.key),
+                  icon: Icons.delete_sweep,
+                  color: Colors.red,
+                ),
+              ],
+              key: Key(entry.key),
+              child: Container(
+                color: theme.canvasColor,
+                child: ListTile(
+                  dense: true,
+                  contentPadding: EdgeInsets.only(left: 0, top: 0),
+                  title: _SectionHeading(entry.key),
+                ),
+              ),
+            ),
+            for (final username in entry.value.keys) ...[
+              Slidable(
+                actionPane: SlidableBehindActionPane(),
+                key: Key('$username@${entry.key}'),
+                secondaryActions: [
+                  IconSlideAction(
+                    closeOnTap: true,
+                    onTap: () => removeUserDialog(entry.key, username),
+                    icon: Icons.delete_sweep,
+                    color: Colors.red,
+                  ),
+                ],
+                child: Container(
+                  decoration: BoxDecoration(color: theme.canvasColor),
+                  child: ListTile(
+                    trailing:
+                        username == accountsStore.defaultUsernameFor(entry.key)
                             ? Icon(
                                 Icons.check_circle_outline,
                                 color: theme.accentColor,
                               )
                             : null,
-                        title: Text(username),
-                        onLongPress: () {
-                          accountsStore.setDefaultAccountFor(
-                              entry.key, username);
-                        },
-                        onTap: () {}, // TODO: go to managing account
-                      ),
-                    ),
-                  ),
-                ],
-                if (entry.value.keys.isEmpty)
-                  ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text('Add account'),
-                    onTap: () {
-                      showCupertinoModalPopup(
-                          context: context,
-                          builder: (_) =>
-                              AddAccountPage(instanceUrl: entry.key));
+                    title: Text(username),
+                    onLongPress: () {
+                      accountsStore.setDefaultAccountFor(entry.key, username);
                     },
+                    onTap: () {}, // TODO: go to managing account
                   ),
-              ]
+                ),
+              ),
             ],
-          );
-        },
+            if (entry.value.keys.isEmpty)
+              ListTile(
+                leading: Icon(Icons.add),
+                title: Text('Add account'),
+                onTap: () {
+                  showCupertinoModalPopup(
+                      context: context,
+                      builder: (_) => AddAccountPage(instanceUrl: entry.key));
+                },
+              ),
+          ]
+        ],
       ),
     );
   }
