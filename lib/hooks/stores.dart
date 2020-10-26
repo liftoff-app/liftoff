8658bf8f123aea7d19c2173a5bd25abbf9ece3fd
shilangyu <xmarcinmarcin@gmail.com>
2020-10-26 09:18:30 +0100

    added store select hooks

--- lib/hooks/stores.dart
+++ lib/hooks/stores.dart
@@ -5,4 +5,9 @@ import '../stores/accounts_store.dart';
 import '../stores/config_store.dart';
 
 AccountsStore useAccountsStore() => useContext().watch<AccountsStore>();
+T useAccountsStoreSelect<T>(T selector(AccountsStore store)) =>
+    useContext().select<AccountsStore, T>(selector);
+
 ConfigStore useConfigStore() => useContext().watch<ConfigStore>();
+T useConfigStoreSelect<T>(T selector(ConfigStore store)) =>
+    useContext().select<ConfigStore, T>(selector);
