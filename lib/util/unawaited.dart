ca47f44e2cdc0f6692bf8ca21383fcc04975bf0c
shilangyu <xmarcinmarcin@gmail.com>
2021-01-03 17:06:40 +0000

    Typo fix

--- lib/util/unawaited.dart
+++ lib/util/unawaited.dart
@@ -1,3 +1,3 @@
 /// Explicitely indicate to the `unawaited_futures` lint
-///  that the future is not await for on purpose
+/// that the future is not awaited for on purpose
 void unawaited<T>(Future<T> future) {}
