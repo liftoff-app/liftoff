bcf6a3363a2d77eeba7ecbceb331422c48d281c3
krawieck <fil.krawczyk@gmail.com>
2021-01-08 17:38:48 +0100

    create search results and search tab pages

---
+++ lib/pages/search_results.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:lemmy_api_client/lemmy_api_client.dart';
+
+import '../comment_tree.dart';
+import '../hooks/stores.dart';
+import '../util/goto.dart';
+import '../widgets/comment.dart';
+import '../widgets/post.dart';
+import '../widgets/sortable_infinite_list.dart';
+
+class SearchResultsPage extends HookWidget {
+  final String instance;
+  final String query;
+
+  SearchResultsPage({
+    @required this.instance,
+    @required this.query,
+  })  : assert(instance != null),
+        assert(query != null),
+        assert(instance.isNotEmpty),
+        assert(query.isNotEmpty);
+
+  @override
+  Widget build(BuildContext context) => DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('Looking for "$query"'),
+            bottom: const TabBar(
+              isScrollable: true,
+              tabs: [
+                Tab(text: 'Posts'),
+                Tab(text: 'Comments'),
+                Tab(text: 'Users'),
+                Tab(text: 'Communities'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              _SearchResultsList(
+                  instance: instance, query: query, type: SearchType.posts),
+              _SearchResultsList(
+                  instance: instance, query: query, type: SearchType.comments),
+              _SearchResultsList(
+                  instance: instance, query: query, type: SearchType.users),
+              _SearchResultsList(
+                  instance: instance,
+                  query: query,
+                  type: SearchType.communities),
+            ],
+          ),
+        ),
+      );
+  //
+}
+
+class _SearchResultsList extends HookWidget {
+  final SearchType type;
+  final String query;
+  final String instance;
+
+  const _SearchResultsList({
+    @required this.type,
+    @required this.query,
+    @required this.instance,
+  })  : assert(type != null),
+        assert(query != null),
+        assert(instance != null);
+
+  @override
+  Widget build(BuildContext context) {
+    final acs = useAccountsStore();
+
+    return SortableInfiniteList(
+      fetcher: (data, page, sort) async {
+        final s = await LemmyApi(instance).v1.search(
+              q: query,
+              sort: sort,
+              type: type,
+              auth: acs.defaultTokenFor(instance)?.raw,
+              page: page,
+            );
+        print(s.users.length.toString());
+        switch (s.type) {
+          case SearchType.comments:
+            return s.comments;
+          case SearchType.communities:
+            return s.communities;
+          case SearchType.posts:
+            return s.posts;
+          case SearchType.users:
+            return s.users;
+          default:
+            throw Error();
+        }
+      },
+      builder: (data) {
+        switch (type) {
+          case SearchType.comments:
+            return Comment(
+              CommentTree(data as CommentView),
+              postCreatorId: null,
+            );
+          case SearchType.communities:
+            // TODO: extract to universal widget
+            return ListTile(
+                title: Text((data as CommunityView).name),
+                onTap: () => goToCommunity.byId(
+                      context,
+                      instance,
+                      (data as CommunityView).id,
+                    ));
+          case SearchType.posts:
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Post(data as PostView),
+            );
+          case SearchType.users:
+            // TODO: extract to universal widget
+            return ListTile(
+                title: Text((data as UserView).name),
+                onTap: () =>
+                    goToUser.byId(context, instance, (data as UserView).id));
+          default:
+            throw Error();
+        }
+      },
+    );
+  }
+}
---
+++ lib/pages/search_tab.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import '../hooks/stores.dart';
+import '../util/goto.dart';
+import '../widgets/bottom_modal.dart';
+import 'search_results.dart';
+
+class SearchTab extends HookWidget {
+  const SearchTab();
+
+  @override
+  Widget build(BuildContext context) {
+    final searchInputController = useTextEditingController();
+
+    final accStore = useAccountsStore();
+    final instance = useState(accStore.instances.first);
+    useValueListenable(searchInputController);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        children: [
+          TextField(
+            controller: searchInputController,
+            autofocus: true,
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              fillColor: Colors.grey,
+              isDense: true,
+              // border: InputBorder.none,
+              contentPadding: const EdgeInsets.symmetric(vertical: 10),
+              hintText: 'search',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Text('instance:',
+                    style: Theme.of(context).textTheme.subtitle1),
+              ),
+              Expanded(
+                child: SelectInstanceButton(
+                  initial: instance.value,
+                  onChange: (s) => instance.value = s,
+                ),
+              ),
+            ],
+          ),
+          if (searchInputController.text.isNotEmpty)
+            ElevatedButton(
+              onPressed: searchInputController.text.isEmpty
+                  ? null
+                  : () => goTo(
+                      context,
+                      (c) => SearchResultsPage(
+                            instance: instance.value,
+                            query: searchInputController.text,
+                          )),
+              child: const Text('search'),
+            )
+        ],
+      ),
+    );
+  }
+}
+
+class SelectInstanceButton extends HookWidget {
+  final void Function(String) onChange;
+  final String initial;
+  const SelectInstanceButton({@required this.onChange, @required this.initial});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final accStore = useAccountsStore();
+    final instance = useState(initial);
+    return OutlinedButton(
+      onPressed: () async {
+        final val = await showModalBottomSheet<String>(
+            backgroundColor: Colors.transparent,
+            isScrollControlled: true,
+            context: context,
+            builder: (context) => BottomModal(
+                  child: Column(
+                    children: [
+                      for (final inst in accStore.instances)
+                        ListTile(
+                          leading: inst == instance.value
+                              ? Icon(
+                                  Icons.radio_button_on,
+                                  color: theme.accentColor,
+                                )
+                              : const Icon(Icons.radio_button_off),
+                          title: Text(inst),
+                          onTap: () => Navigator.of(context).pop(inst),
+                        )
+                    ],
+                  ),
+                ));
+        if (val != null) {
+          instance.value = val;
+          onChange(val);
+        }
+      },
+      style: OutlinedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        primary: theme.textTheme.bodyText1.color,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(instance.value),
+          const Icon(Icons.arrow_drop_down),
+        ],
+      ),
+    );
+  }
+}
