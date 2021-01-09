0bef4fe1afc7e190d3d0027cfc8374b6ba26b9e6
krawieck <fil.krawczyk@gmail.com>
2021-01-09 02:36:27 +0100

    add vibration of refresh to communities tab

--- lib/pages/communities_tab.dart
+++ lib/pages/communities_tab.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:fuzzy/fuzzy.dart';
 import 'package:lemmy_api_client/lemmy_api_client.dart';
@@ -94,6 +95,7 @@ class CommunitiesTab extends HookWidget {
     }
 
     refresh() async {
+      await HapticFeedback.mediumImpact();
       try {
         final i = getInstances();
         final c = getCommunities();
