b9ddb23d235cd53eba4a6405c6a03e858e4cd706
ryg-git <ygretharekar@outlook.com>
2021-02-06 19:34:11 +0530

    removed unused import

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,5 +1,3 @@
-import 'dart:ui' as ui;
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:esys_flutter_share/esys_flutter_share.dart';
 import 'package:flutter/cupertino.dart';
