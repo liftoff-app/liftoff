3afb5c9d116f1cf13f60dac8a130e95b41d3fd12
krawieck <fil.krawczyk@gmail.com>
2021-02-13 17:03:40 +0100

    remove offset cuz it got fixed in lac

--- lib/util/extensions/datetime.dart
+++ lib/util/extensions/datetime.dart
@@ -2,11 +2,8 @@ import 'package:timeago/timeago.dart' as timeago;
 
 extension FancyTime on DateTime {
   /// returns `this` time as a relative, human-readable string. In short format
-  String get fancyShort => timeago.format(this,
-      locale: 'en_short',
-      clock: DateTime.now().subtract(DateTime.now().timeZoneOffset));
+  String get fancyShort => timeago.format(this, locale: 'en_short');
 
   /// returns `this` time as a relative, human-readable string
-  String get fancy => timeago.format(this,
-      clock: DateTime.now().subtract(DateTime.now().timeZoneOffset));
+  String get fancy => timeago.format(this);
 }
