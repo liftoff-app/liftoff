c5f75c94ad0209a7e8199cad0aed6a059b3a161a
ryg-git <ygretharekar@outlook.com>
2021-04-01 00:58:46 +0530

    handle FormatException thrown by Uri.parse

--- lib/util/cleanup_url.dart
+++ lib/util/cleanup_url.dart
@@ -1,7 +1,12 @@
 /// Returns host of a url without a leading 'www.' or protocol if present also
 /// removes trailing '/'
-String cleanUpUrl(String url) =>
-    urlHost(url.startsWith('https://') ? url : 'https://$url');
+String cleanUpUrl(String url) {
+  try {
+    return urlHost(url.startsWith('https://') ? url : 'https://$url');
+  } on FormatException catch (_) {
+    return '';
+  }
+}
 
 // Returns host of a url without a leading 'www.' if present
 String urlHost(String url) {
