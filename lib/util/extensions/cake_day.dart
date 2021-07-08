45ea7a0b546b34767eb50c96d0a7a01135a41de8
krawieck <fil.krawczyk@gmail.com>
2021-07-08 13:57:48 +0200

    add `.toUtc()`

--- lib/util/extensions/cake_day.dart
+++ lib/util/extensions/cake_day.dart
@@ -3,7 +3,7 @@ import 'package:lemmy_api_client/v3.dart';
 // inspired by https://github.com/LemmyNet/lemmy-ui/blob/66c846ededef8c0afd5aaadca4aaedcbaeab3ee6/src/shared/utils.ts#L533
 extension PersonSafeCakeDay on PersonSafe {
   bool get isCakeDay {
-    final now = DateTime.now();
+    final now = DateTime.now().toUtc();
 
     return now.day == published.day &&
         now.month == published.month &&
