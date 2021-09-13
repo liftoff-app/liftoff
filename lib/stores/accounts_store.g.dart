8c69964657f95049b2aa596baa882f0d08cf1f96
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2021-09-13 23:37:08 +0200

    Regenerate gen files (#254)

--- lib/stores/accounts_store.g.dart
+++ lib/stores/accounts_store.g.dart
@@ -6,23 +6,23 @@ part of 'accounts_store.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-AccountsStore _$AccountsStoreFromJson(Map<String, dynamic> json) {
-  return AccountsStore()
-    ..accounts = (json['accounts'] as Map<String, dynamic>?)?.map(
-          (k, e) => MapEntry(
-              k,
-              (e as Map<String, dynamic>).map(
-                (k, e) =>
-                    MapEntry(k, UserData.fromJson(e as Map<String, dynamic>)),
-              )),
-        ) ??
-        {'lemmy.ml': {}}
-    ..defaultAccounts = (json['defaultAccounts'] as Map<String, dynamic>?)?.map(
-          (k, e) => MapEntry(k, e as String),
-        ) ??
-        {}
-    ..defaultAccount = json['defaultAccount'] as String?;
-}
+AccountsStore _$AccountsStoreFromJson(Map<String, dynamic> json) =>
+    AccountsStore()
+      ..accounts = (json['accounts'] as Map<String, dynamic>?)?.map(
+            (k, e) => MapEntry(
+                k,
+                (e as Map<String, dynamic>).map(
+                  (k, e) =>
+                      MapEntry(k, UserData.fromJson(e as Map<String, dynamic>)),
+                )),
+          ) ??
+          {'lemmy.ml': {}}
+      ..defaultAccounts =
+          (json['defaultAccounts'] as Map<String, dynamic>?)?.map(
+                (k, e) => MapEntry(k, e as String),
+              ) ??
+              {}
+      ..defaultAccount = json['defaultAccount'] as String?;
 
 Map<String, dynamic> _$AccountsStoreToJson(AccountsStore instance) =>
     <String, dynamic>{
@@ -31,12 +31,10 @@ Map<String, dynamic> _$AccountsStoreToJson(AccountsStore instance) =>
       'defaultAccount': instance.defaultAccount,
     };
 
-UserData _$UserDataFromJson(Map<String, dynamic> json) {
-  return UserData(
-    jwt: Jwt.fromJson(json['jwt'] as String),
-    userId: json['userId'] as int,
-  );
-}
+UserData _$UserDataFromJson(Map<String, dynamic> json) => UserData(
+      jwt: Jwt.fromJson(json['jwt'] as String),
+      userId: json['userId'] as int,
+    );
 
 Map<String, dynamic> _$UserDataToJson(UserData instance) => <String, dynamic>{
       'jwt': instance.jwt,
