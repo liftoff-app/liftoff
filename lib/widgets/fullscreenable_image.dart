9204380ef9ed19a9e68fef3e6d51246ef1f635bb
Cody Goldberg <cody@goldberg.fyi>
2023-07-02 23:15:29 -0400

    Consistent post media sizes (#219)
    This PR aims to improve some small UX aspects around "endless scrolling"
    while `PostMedia` loads images.
    
    Previously, images within posts would size up to fit the width of the
    list, with an unconstrained height. This led to cases where images up in
    the list could push content down if and when they load late. A typical
    real-world scenario could include a slower than normal internet
    connection; if a user is reading some content below where an image would
    normally be, and this image loads late, their "reading position" will be
    snapped downward by the loaded image.
    
    These changes include setting a constant size for images, loaded or
    unloaded, to be 300dp. Once an image loads in, it is clipped and fit to
    be contained within this boundary. Tapping on the image, or otherwise
    viewing the entire post in full, will display the entire image
    unclipped.
    
    The following video illustrates the clipped image, as it transitions as
    a hero to the full screen preview.
    
    
    https://github.com/liftoff-app/liftoff/assets/459941/2a9220cb-b8cd-4301-81ed-e83c42827822
    
    The following video illustrates the constant height while an image is
    loading.
    
    
    https://github.com/liftoff-app/liftoff/assets/459941/01895929-9a73-44cb-ac69-f6e2e3b464e2
    
    Co-authored-by: swmarks <swmarks@users.noreply.github.com>

--- lib/widgets/fullscreenable_image.dart
+++ lib/widgets/fullscreenable_image.dart
@@ -3,6 +3,7 @@ import 'package:uuid/uuid.dart';
 
 import '../pages/media_view.dart';
 import '../util/goto.dart';
+import 'clipped_image.dart';
 
 /// If the media is pressed, it opens itself in a [MediaViewPage]
 class FullscreenableImage extends StatelessWidget {
@@ -16,11 +17,70 @@ class FullscreenableImage extends StatelessWidget {
     required this.child,
   });
 
+  static RectTween _createRectTween(Rect? begin, Rect? end) {
+    return MaterialRectCenterArcTween(begin: begin, end: end);
+  }
+
+  static Widget _flightShuttleBuilder(
+    BuildContext flightContext,
+    Animation<double> animation,
+    HeroFlightDirection flightDirection,
+    BuildContext fromHeroContext,
+    BuildContext toHeroContext,
+  ) {
+    final toHero = toHeroContext.widget as Hero;
+
+    final toMediaQueryData = MediaQuery.maybeOf(toHeroContext);
+    final fromMediaQueryData = MediaQuery.maybeOf(fromHeroContext);
+
+    if (toMediaQueryData == null || fromMediaQueryData == null) {
+      return toHero.child;
+    }
+
+    final fromHeroPadding = fromMediaQueryData.padding;
+    final toHeroPadding = toMediaQueryData.padding;
+
+    Widget widget = AnimatedBuilder(
+      animation: animation,
+      builder: (BuildContext context, Widget? child) {
+        return MediaQuery(
+          data: toMediaQueryData.copyWith(
+            padding: (flightDirection == HeroFlightDirection.push)
+                ? EdgeInsetsTween(
+                    begin: fromHeroPadding,
+                    end: toHeroPadding,
+                  ).evaluate(animation)
+                : EdgeInsetsTween(
+                    begin: toHeroPadding,
+                    end: fromHeroPadding,
+                  ).evaluate(animation),
+          ),
+          child: toHero.child,
+        );
+      },
+    );
+
+    /// We construct a custom flightShuttleBuilder primarily for this reason;
+    /// the "pushed" direction, or tree that includes `MediaViewPage` uses
+    /// a `PhotoView` widget. We're unable to safely apply `FitClippedWidget`
+    /// as a destination.
+    ///
+    /// Therefore, only apply the FitClippedWidget while we're in flight. Once
+    /// this hero animation is over, the underlying `PhotoView` is utiilized
+    /// instead.
+    if (flightDirection == HeroFlightDirection.push) {
+      widget = FitClippedWidget(child: widget);
+    }
+    return widget;
+  }
+
   @override
   Widget build(BuildContext context) => InkWell(
         onTap: () => goToMedia(context, url, heroTag),
         child: Hero(
           tag: heroTag,
+          createRectTween: _createRectTween,
+          flightShuttleBuilder: _flightShuttleBuilder,
           child: child,
         ),
       );
