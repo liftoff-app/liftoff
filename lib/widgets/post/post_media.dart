19f7c98f4346c3c36fb79f96b5a8a772f4934e46
Zach Russell <zachatrocity@gmail.com>
2023-07-05 22:59:47 -0600

    Revert "Consistent post media sizes (#219)"
    This reverts commit 9204380ef9ed19a9e68fef3e6d51246ef1f635bb.

--- lib/widgets/post/post_media.dart
+++ lib/widgets/post/post_media.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../util/observer_consumers.dart';
 import '../cached_network_image.dart';
-import '../clipped_image.dart';
 import '../fullscreenable_image.dart';
-import 'post_status.dart';
 import 'post_store.dart';
 
 /// assembles image
@@ -17,38 +15,16 @@ class PostMedia extends StatelessWidget {
       builder: (context, store) {
         final post = store.postView.post;
         if (!store.hasMedia) return const SizedBox();
-        final isFullPost = context.read<IsFullPost>();
 
         final url = post.url!; // hasMedia returns false if url is null
 
         return FullscreenableImage(
           url: url,
-          child: SizedBox(
-            height: !isFullPost ? 300 : null,
-            child: CachedNetworkImage(
-              imageUrl: url,
-              errorBuilder: (_, ___) => const Center(
-                child: Icon(Icons.warning),
-              ),
-              loadingBuilder: (context, progress) => Center(
-                child: CircularProgressIndicator.adaptive(
-                  value: progress?.progress,
-                ),
-              ),
-              imageBuilder: (context, imageProvider) {
-                if (isFullPost) {
-                  return Image(
-                    image: imageProvider,
-                  );
-                }
-
-                return FitClippedWidget(
-                  child: Image(
-                    image: imageProvider,
-                  ),
-                );
-              },
-            ),
+          child: CachedNetworkImage(
+            imageUrl: url,
+            errorBuilder: (_, ___) => const Icon(Icons.warning),
+            loadingBuilder: (context, progress) =>
+                CircularProgressIndicator.adaptive(value: progress?.progress),
           ),
         );
       },
