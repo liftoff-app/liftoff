ab3a388d04df2a132c78a8c8155030ca6d4ec4c7
@mykdavies <mykdavies@gmail.com>
2023-07-12 17:42:58 +0200

    New Code of Conduct plus tidy up

--- lib/widgets/about_tile.dart
+++ lib/widgets/about_tile.dart
@@ -5,7 +5,6 @@ import 'package:package_info_plus/package_info_plus.dart';
 
 import '../gen/assets.gen.dart';
 import '../hooks/memo_future.dart';
-import '../pages/log_console/log_console.dart';
 import '../resources/links.dart';
 import '../url_launcher.dart';
 import 'bottom_safe.dart';
@@ -21,14 +20,22 @@ class AboutTile extends HookWidget {
     final assetBundle = DefaultAssetBundle.of(context);
     final changelogSnap =
         useMemoFuture(() => assetBundle.loadString('CHANGELOG.md'));
+    final codeOfConductSnap =
+        useMemoFuture(() => assetBundle.loadString('CODE_OF_CONDUCT.md'));
+    final privacySnap =
+        useMemoFuture(() => assetBundle.loadString('PRIVACY_POLICY.md'));
 
     final packageInfo = packageInfoSnap.data;
     final changelog = changelogSnap.data;
+    final codeOfConduct = codeOfConductSnap.data;
+    final privacyPolicy = privacySnap.data;
 
     if (!packageInfoSnap.hasData ||
         !changelogSnap.hasData ||
         packageInfo == null ||
-        changelog == null) {
+        changelog == null ||
+        codeOfConduct == null ||
+        privacyPolicy == null) {
       return const SizedBox.shrink();
     }
 
@@ -39,23 +46,34 @@ class AboutTile extends HookWidget {
             'A client for Lemmy, written in Flutter.\n\nBased on the Lemmur project.'),
         const SizedBox(height: 40),
         FilledButton.icon(
+            icon: const Icon(Icons.rule_rounded),
+            label: const Text('Code of Conduct'),
+            onPressed: () => Navigator.of(context).push(
+                DisplayDocumentPage.route('Code of Conduct', codeOfConduct))),
+        FilledButton.icon(
+            icon: const Icon(Icons.policy_outlined),
+            label: const Text('Privacy Policy'),
+            onPressed: () => Navigator.of(context).push(
+                DisplayDocumentPage.route('Privacy Policy', privacyPolicy))),
+        FilledButton.icon(
             icon: const Icon(Icons.subject),
             label: const Text('changelog'),
-            onPressed: () =>
-                Navigator.of(context).push(ChangelogPage.route(changelog))),
+            onPressed: () => Navigator.of(context)
+                .push(DisplayDocumentPage.route('changelog', changelog))),
         FilledButton.icon(
           icon: const Icon(Icons.code),
           label: const Text('source code'),
           onPressed: () =>
               launchLink(link: liftoffRepositoryUrl, context: context),
         ),
-        FilledButton.icon(
-          icon: const Icon(Icons.list_alt),
-          label: const Text('logs'),
-          onPressed: () {
-            Navigator.of(context).push(LogConsolePage.route());
-          },
-        ),
+        // Never seen a log entry, so let's not bother with this right now.
+        // FilledButton.icon(
+        //   icon: const Icon(Icons.list_alt),
+        //   label: const Text('logs'),
+        //   onPressed: () {
+        //     Navigator.of(context).push(LogConsolePage.route());
+        //   },
+        // ),
       ],
       applicationIcon: ClipRRect(
         borderRadius: BorderRadius.circular(10),
@@ -66,25 +84,37 @@ class AboutTile extends HookWidget {
   }
 }
 
-class ChangelogPage extends StatelessWidget {
-  final String changelog;
+class DisplayDocumentPage extends StatelessWidget {
+  final String title;
+  final String contents;
 
-  const ChangelogPage(this.changelog, {super.key});
+  const DisplayDocumentPage(this.title, this.contents, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Changelog')),
+        appBar: AppBar(title: Text(title)),
         body: ListView(
           padding: const EdgeInsets.all(16),
           children: [
-            MarkdownBody(data: changelog),
+            MarkdownBody(
+              data: contents,
+              onTapLink: (text, href, title) async {
+                final didLaunch = await launchLink(
+                  link: href!,
+                  context: context,
+                );
+                if (didLaunch) {
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
             const BottomSafe(),
           ],
         ));
   }
 
-  static Route route(String changelog) => MaterialPageRoute(
-        builder: (context) => ChangelogPage(changelog),
+  static Route route(String title, String contents) => MaterialPageRoute(
+        builder: (context) => DisplayDocumentPage(title, contents),
       );
 }
