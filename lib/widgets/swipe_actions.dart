1892a3f03bd18fdb3675303d4107b110272f471a
Lasath Fernando <devel@lasath.org>
2023-07-12 20:53:20 -0400

    Add swipe gesture actions (#359)
    I tried to add a video but the recordings my android emulator took was
    unplayable on any machine for some reason :shrug:
    
    This adds two actions (upvote and save) by swiping left. Downvotes still
    require you to be mad enough to actually press the button :grin:
    We can make them user-configurable at a future date - I tried to make
    things extensible.
    Admittedly I don't know what the best practices are regarding
    flutter/dart specifically, but I:
    - created an abstract `LiftoffAction` that represents something a user
    can do
    - think we should refactor the buttons in the post/comment cards to use
    those actions.
    
    I couldn't use the `Dismissable` widget because I wanted it to select
    one of multiple actions based on how far the user has swiped. Thus, I
    created a new widget called `WithSwipeActions` (open to suggestions on
    the name).

---
+++ lib/widgets/swipe_actions.dart
@@ -0,0 +1,83 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import '../liftoff_action.dart';
+
+/// Widget that wraps [child] and allows for swipe actions to be performed on it.
+/// [actions] are the actions that can be performed, and [onTrigger] is called
+/// when the user triggers an action.
+class WithSwipeActions extends HookWidget {
+  final Widget child;
+  final List<LiftoffAction> actions;
+  final void Function(LiftoffAction) onTrigger;
+
+  const WithSwipeActions(
+      {super.key,
+      required this.child,
+      required this.actions,
+      required this.onTrigger});
+
+  @override
+  Widget build(BuildContext context) {
+    final animationController =
+        useAnimationController(duration: const Duration(milliseconds: 300));
+    final slideAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(-1, 0),
+    ).animate(animationController);
+
+    // This starts at -1, and must be dragged to > 0 (at least 2 spaces) to
+    // trigger an action That way, the user has a chance to see the action
+    // without triggering it
+    final activeActionIndex = useState(-1);
+    final activeAction = activeActionIndex.value > 0
+        ? actions[min(activeActionIndex.value, actions.length) - 1]
+        : null;
+
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onHorizontalDragUpdate: (details) {
+        final newPosition = animationController.value -
+            details.primaryDelta! / MediaQuery.of(context).size.width;
+
+        // each action is 1/5 of the screen
+        final newActiveActionIndex = newPosition ~/ 0.2;
+        if (newActiveActionIndex <= actions.length) {
+          animationController.value = newPosition;
+          activeActionIndex.value = newActiveActionIndex;
+        }
+      },
+      onHorizontalDragEnd: (details) {
+        // only trigger if the user has dragged far enough
+        if (activeAction != null) {
+          onTrigger(activeAction);
+        }
+
+        // reset and put the widget back to normal
+        activeActionIndex.value = -1;
+        animationController.reverse();
+      },
+      child: Stack(children: [
+        if (activeAction != null)
+          Positioned.fill(
+            child: ColoredBox(
+                color: activeAction.activeColor,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        right: MediaQuery.of(context).size.width / 10),
+                    child: Icon(activeAction.icon),
+                  ),
+                )),
+          ),
+        SlideTransition(
+          position: slideAnimation,
+          child: child,
+        ),
+      ]),
+    );
+  }
+}
