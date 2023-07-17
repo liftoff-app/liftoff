51a351c576c35215b33f584fbeb46c1200d0778a
Lasath Fernando <devel@lasath.org>
2023-07-17 18:31:15 +0000

    Fix linter error

--- lib/widgets/infinite_scroll.dart
+++ lib/widgets/infinite_scroll.dart
@@ -1,5 +1,3 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
