liftoff-app/liftoff

commit 0b6137f6968d4719c31b93131a5cf55148c8db38

De-duplicate contents of infinite scroll (#441)
Addresses a `TODO` comment I left in #424.
Also improves performance (probably because there's now less renders.

lib/widgets/infinite_scroll.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
@@ -49,7 +51,7 @@ class InfiniteScroll<T> extends HookWidget {
 
   /// Maps an item to its unique property that will allow to detect possible
   /// duplicates thus perfoming deduplication
-  final Object Function(T item)? uniqueProp;
+  final Object Function(T item) uniqueProp;
 
   /// If true, all content will be discarded and refetched when value
   /// of [fetcher] changes.
@@ -70,7 +72,7 @@ class InfiniteScroll<T> extends HookWidget {
     this.controller,
     this.noItems = const SizedBox.shrink(),
     this.refreshOnFetcherUpdate = false,
-    this.uniqueProp,
+    required this.uniqueProp,
   }) : assert(batchSize > 0);
 
   @override
@@ -87,19 +89,30 @@ class InfiniteScroll<T> extends HookWidget {
       return null;
     }, []);
 
+    final dataSet = useRef(HashSet<Object>());
+
     // Need to memoize the callback so we get a single instance
     // that we can add/remove from the controller.
     final pageRequestListener = useCallback((pageKey) async {
       try {
-        final items = await fetcher(pageKey, batchSize);
-        // TODO: check if deduplication is needed
-        // final uniqueItems =
-        //     uniqueProp == null ? items : LinkedHashSet<T>.from(items).toList();
-        final isLastPage = items.length < batchSize;
+        final newItems = await fetcher(pageKey, batchSize);
+        final uniqueNewItems = newItems.where((item) {
+          final uniquePropValue = uniqueProp(item);
+          if (dataSet.value.contains(uniquePropValue)) {
+            return false;
+          }
+
+          dataSet.value.add(uniquePropValue);
+          return true;
+        }).toList();
+
+        // If we got less than `batchSize` items, then we reached the end.
+        // Note: we're checking `newItems` and not `uniqueNewItems`.
+        final isLastPage = newItems.length < batchSize;
         if (isLastPage) {
-          pagingController.appendLastPage(items);
+          pagingController.appendLastPage(uniqueNewItems);
         } else {
-          pagingController.appendPage(items, pageKey + 1);
+          pagingController.appendPage(uniqueNewItems, pageKey + 1);
         }
       } catch (error) {
         pagingController.error = error;

lib/widgets/post/post.dart
@@ -87,7 +87,6 @@ class _Post extends HookWidget {
       onTap: isFullPost
           ? null
           : () {
-              final postStore = context.read<PostStore>();
               Navigator.of(context)
                   .push(FullPostPage.fromPostStoreRoute(postStore));
             },

lib/widgets/sortable_infinite_list.dart
@@ -30,7 +30,7 @@ class SortableInfiniteList<T> extends HookWidget {
   /// if no defaultSort is provided, the defaultSortType
   /// from the configStore will be used
   final SortType? defaultSort;
-  final Object Function(T item)? uniqueProp;
+  final Object Function(T item) uniqueProp;
   const SortableInfiniteList({
     super.key,
     required this.fetcher,
@@ -40,7 +40,7 @@ class SortableInfiniteList<T> extends HookWidget {
     this.noItems = const SizedBox.shrink(),
     this.defaultSort,
     this.refreshOnFetcherUpdate = false,
-    this.uniqueProp,
+    required this.uniqueProp,
   });
 
   @override
@@ -88,7 +88,7 @@ class SortableInfiniteCommentList<T> extends HookWidget {
   /// if no defaultSort is provided, the defaultSortType
   /// from the configStore will be used
   final dynamic defaultSort;
-  final Object Function(T item)? uniqueProp;
+  final Object Function(T item) uniqueProp;
   const SortableInfiniteCommentList({
     super.key,
     required this.fetcher,
@@ -97,7 +97,7 @@ class SortableInfiniteCommentList<T> extends HookWidget {
     this.onStyleChange,
     this.noItems = const SizedBox.shrink(),
     this.defaultSort,
-    this.uniqueProp,
+    required this.uniqueProp,
   });
 
   @override

commit 8fa4645c891bf8b40ec85759b8419e5b751aadf4

Make list refresh work again (#443)
Fixes regression caused in #441

lib/widgets/infinite_scroll.dart
@@ -80,7 +80,17 @@ class InfiniteScroll<T> extends HookWidget {
     final pagingController =
         useMemoized(() => PagingController<int, T>(firstPageKey: 1), []);
 
+    final dataSet = useRef(HashSet<Object>());
+
     useEffect(() {
+      pagingController.addStatusListener((status) {
+        // if there are less rendered items than unique items, we've probably
+        // refreshed the page. Rebuilding the set will probably be a no-op.
+        if ((pagingController.itemList?.length ?? 0) < dataSet.value.length) {
+          dataSet.value =
+              HashSet.from(pagingController.itemList?.map(uniqueProp) ?? []);
+        }
+      });
       controller?.clear = pagingController.refresh;
       controller?.scrollToTop = () => PrimaryScrollController.of(context)
           .animateTo(0,
@@ -88,8 +98,6 @@ class InfiniteScroll<T> extends HookWidget {
               curve: Curves.easeInOut);
       return null;
     }, []);
-
-    final dataSet = useRef(HashSet<Object>());
 
     // Need to memoize the callback so we get a single instance
     // that we can add/remove from the controller.